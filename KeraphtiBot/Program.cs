4649cf54fffab3f09f6241ffd23a738397d3cf52
C(rafty)# <76676876+Crafty311@users.noreply.github.com>
2021-06-22 02:33:31 +0600

    Add files via upload

---
+++ KeraphtiBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace KeraphtiBot
+{
+    class Program
+    {
+        private static void Main(string[] args) 
+        {
+            Bot bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+
+        }
+    }
+}
